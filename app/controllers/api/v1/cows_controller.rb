7d2960f029d464640157a7947352715c357daa1a
Craig Kuriger <craig.kuriger@crssoftware.co.nz>
2015-12-22 13:57:01 +1300

    update update method

--- app/controllers/api/v1/cows_controller.rb
+++ app/controllers/api/v1/cows_controller.rb
@@ -2,31 +2,6 @@ class Api::V1::CowsController < ApplicationController
 
 skip_before_action :verify_authenticity_token
 
-before_filter :cors_preflight_check
-after_filter :cors_set_access_control_headers
-
-  # For all responses in this controller, return the CORS access control headers.
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  # If this is a preflight OPTIONS request, then short-circuit the
-  # request, return only the necessary headers and return an empty
-  # text/plain.
-
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
-
   def index
   	render json: Cow.all.as_json(include: :calves)
   end
@@ -62,22 +37,12 @@ after_filter :cors_set_access_control_headers
   end
 
   def update
-    respond_to do |format|
+    @cow = Cow.find_by_id(params[:id])
       if @cow.update(cow_params)
-        format.html { redirect_to @cow, notice: 'Cow was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cow }
-      else
-        format.html { render :edit }
-        format.json { render json: @cow.errors, status: :unprocessable_entity }
-      end
+      render json: @cow.as_json
+    else
+      render status: 400
     end
-  end
-
-  def destroy
-    @cow.destroy
-    respond_to do |format|
-      format.html { redirect_to cows_url, notice: 'Cow was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
